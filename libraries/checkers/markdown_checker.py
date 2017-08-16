2dcd366b974c9a9ea4d0341f1e5b962a2c4d623b
Richard Mahn <rich.mahn@unfoldingword.org>
2017-08-16 13:58:48 -0600

    Testing code

--- libraries/checkers/markdown_checker.py
+++ libraries/checkers/markdown_checker.py
@@ -20,15 +20,21 @@ class MarkdownChecker(Checker):
         """
         lambda_handler = LambdaHandler()
         lint_function = '{0}tx_markdown_linter'.format(self.prefix)
+        print("######### " + lint_function)
         for f in sorted(get_files(directory=self.preconvert_dir, extensions=['md'])):
+            print("######### "+f)
             filename = os.path.basename(f)
             response = lambda_handler.invoke(lint_function, {'options': {'strings': {filename: read_file(f)}}})
+            print("######### {}".format(response))
             if 'errorMessage' in response:
                 self.log.error(response['errorMessage'])
+                print("######### {}".format(response['errorMessage']))
             elif 'Payload' in response:
                 lint_data = json.loads(response['Payload'].read())
+                print("######### {}".format(lint_data))
                 for lint in lint_data[filename]:
                     line = '{0}:{1}:{2}: {3} (Text: {4})[{5}]'. \
                         format(filename, lint['lineNumber'], lint['ruleAlias'], lint['ruleDescription'],
                                lint['errorContext'], lint['ruleName'])
+                    print("######### {}".format(line))
                     self.log.warning(line)
